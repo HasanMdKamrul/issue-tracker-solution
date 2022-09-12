956785cd6fe230bd8cd4e4fe822c87c7543db750
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-12 19:25:10 +0200

    If no prior issu found length null problem fixed

--- main.js
+++ main.js
@@ -37,20 +37,27 @@ const deleteIssue = id => {
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
+  // console.log(issues)
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
 
-  for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
-
-    issuesList.innerHTML +=   `<div class="well">
-                              <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
-                              </div>`;
+  if (!issues) {
+      return;
+  } else{
+    for (var i = 0; i < issues.length; i++) {
+      const {id, description, severity, assignedTo, status} = issues[i];
+  
+      issuesList.innerHTML +=   `<div class="well">
+                                <h6>Issue ID: ${id} </h6>
+                                <p><span class="label label-info"> ${status} </span></p>
+                                <h3> ${description} </h3>
+                                <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                                <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                                <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                                <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                                </div>`;
+    }
   }
+
+ 
 }
